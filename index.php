740be9165a9baec426e03cf42583d2616cbcae44
Litzy-12 <65592160+Litzy-12@users.noreply.github.com>
2021-05-03 17:57:14 -0500

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
   <LINK REL="stylesheet" TYPE="text/css" HREF="css/estilo_index.css">
 </head>
 
-<body background = "Imagenes/fondo_index.png">
+<body background = "Imagenes/fondo_index.png" style="background-size:50% 50%;">
 
 <div class="container">
 
