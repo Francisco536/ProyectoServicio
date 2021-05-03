b17a1932a047f1fb25323ef7655b925444ce9120
Litzy-12 <65592160+Litzy-12@users.noreply.github.com>
2021-05-03 10:19:25 -0500

    Update Menu.php

--- Models/Numeros/Menu.php
+++ Models/Numeros/Menu.php
@@ -30,7 +30,7 @@ if(!isset($usuario)){
     <a href="../menu.php" style=" background: url(Imagenes/Menu/btnSalir.png) /* Url de la imagen */ no-repeat center center, #62BC0F /* Color del botón */; background-size: 105%; height: 80px;  /* Alto del botón */
   width: 100px;  /* Ancho del botón */"class="boton"></a> 
   
-<a href="../menu.php" style=" background: url(Imagenes/041-home.PNG) /* Url de la imagen */ no-repeat center center, 
+<a href="../menu.php" style=" background: url(Imagenes/041-home.png) /* Url de la imagen */ no-repeat center center, 
  background-size: 50%; height: 75px;  /* Alto del botón */
   width: 110px;  /* Ancho del botón */"class="boton1" ></a>
    </center>
