0146aa235fe6751c844399817a9850b9900fdfad
Litzy-12 <65592160+Litzy-12@users.noreply.github.com>
2021-05-06 22:29:20 -0500

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
   <LINK REL="stylesheet" TYPE="text/css" HREF="css/estilo_index.css">
 </head>
 
-<body background = "Imagenes/fondo_index.png" style="background-size:100% 130%; top:300px; left:290px;">
+<body background = "Imagenes/fondo_index.png" style="background-size:100% 130%; top:300px; left:270px;">
 
 
              <div class="modal-footer">
