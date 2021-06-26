303b102b05b9248c4b6d844ab83fcb7ffd660de6
Freddy50309 <64513432+Freddy50309@users.noreply.github.com>
2021-06-26 13:46:14 -0500

    Update inicio.php
    Cambio de Fondo

--- inicio.php
+++ inicio.php
@@ -2,21 +2,45 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-
-	<meta charset="utf-8">
-
+  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <LINK REL="stylesheet" TYPE="text/css" HREF="css/estilo_index.css">
+  <LINK REL="stylesheet" TYPE="text/css" HREF="css/estilo_inicio.css">
 </head>
 
 <body>
 
+<div class="container">
+    <div class="login-wrap">
+        <div class="login-html">
 
-             <div class="modal-footer">
-             <big><big> <big><big> <b> <a href="inicio.php" class="boton verde">JUGAR</a></b></big></big></big></big>
-             
-             </div>
+          <div class="login-form">
+            <div class="sign-in-htm">
+              <div class="group">
 
+                 <br></br>
+                 <br></br>
+                 <br></br>
+                 <br></br>
+                 <br></br>
+
+                <div class="modal-footer">
+                  <ul></ul>
+                  <ul></ul>
+                 <b>
+                   <big><big> <a href="Registro.php" class="boton verde">REGISTRARSE</a></big></big>
+                  <br>
+                  <big><big> <a href="login.php" class="boton azul">CONTINUAR</a></big></big>
+                  <br>
+                  <big><big> <a href="index.php" class="boton rojo">SALIR</a></big></big>
+                  
+                </b>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+</div>
 </body>
 </html>
