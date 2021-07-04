22be7097e30cfad354394519a0c735af63c06f8e
Litzy-12 <65592160+Litzy-12@users.noreply.github.com>
2021-07-04 14:08:51 -0500

    Update Menu.php

--- Models/Figuras/Menu.php
+++ Models/Figuras/Menu.php
@@ -25,9 +25,9 @@ if(!isset($usuario)){
  <center>
  <br><br>
     <a href="Seleccion.php" style=" background: url(Imagenes/Menu/btnInicio.png) /* Url de la imagen */ no-repeat center center, #62BC0F /* Color del botón */; background-size: 105%; height: 150px;  /* Alto del botón */
-  width: 150px; margin:170px;  /* Ancho del botón */"class="boton" ></a>
+  width: 150px; margin:200px -50px;   /* Ancho del botón */"class="boton" ></a>
     <a href="../menu.php" style=" background: url(Imagenes/Menu/btnSalir.png) /* Url de la imagen */ no-repeat center center, #62BC0F /* Color del botón */; background-size: 105%; height: 80px;  /* Alto del botón */
-  width: 100px; margin:170px; /* Ancho del botón */"class="boton"></a> 
+  width: 100px; margin:200px -50px; /* Ancho del botón */"class="boton"></a> 
   
 <a href="../menu.php" style=" background: url(Imagenes/041-home.png) no-repeat center center;
  background-size: 50%; height: 75px;
