ec4a5892844abb3b561f386fef3631a593228dcd
Litzy-12 <65592160+Litzy-12@users.noreply.github.com>
2021-07-04 14:22:29 -0500

    Update Menu.php

--- Models/Figuras/Menu.php
+++ Models/Figuras/Menu.php
@@ -26,12 +26,12 @@ if(!isset($usuario)){
  <br><br>
     <a href="Seleccion.php" style=" background: url(Imagenes/Menu/btnInicio.png) /* Url de la imagen */ no-repeat center center, #62BC0F /* Color del botón */; background-size: 105%; height: 150px;  /* Alto del botón */
   width: 150px; margin:240px 10px;   /* Ancho del botón */"class="boton" ></a>
-    <a href="../menu.php" style=" background: url(Imagenes/Menu/btnSalir.png) /* Url de la imagen */ no-repeat center center, #62BC0F /* Color del botón */; background-size: 105%; height: 80px;  /* Alto del botón */
-  width: 100px; margin:240px 10px; /* Ancho del botón */"class="boton"></a> 
+    <a href="../menu.php" style=" background: url(Imagenes/Menu/btnSalir.png) /* Url de la imagen */ no-repeat center center, #62BC0F /* Color del botón */; background-size: 105%; height: 150px;  /* Alto del botón */
+  width: 150px; margin:240px 10px; /* Ancho del botón */"class="boton"></a> 
   
 <a href="../menu.php" style=" background: url(Imagenes/041-home.png) no-repeat center center;
  background-size: 50%; height: 75px;
-  width: 110px;" class="leekids" ></a>
+  width: 110px; margin:70px 50px;" class="leekids" ></a>
    </center>
  
     </head>
