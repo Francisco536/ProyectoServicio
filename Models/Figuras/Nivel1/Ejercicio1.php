0339adf2a003cafbfefe67628a6bdf93e9152aab
Litzy-12 <65592160+Litzy-12@users.noreply.github.com>
2021-07-04 19:58:43 -0500

    Update Ejercicio1.php

--- Models/Figuras/Nivel1/Ejercicio1.php
+++ Models/Figuras/Nivel1/Ejercicio1.php
@@ -32,9 +32,10 @@ height: 215px;  /* Alto del botón */
 background-size: 135%;
  height: 90px;  /* Alto del botón */
   width: 100px;  /* Ancho del botón */"class="boton4"></a> 
-   <a href="../../menu.php" style=" background: url(../Imagenes/este.PNG) /* Url de la imagen */ no-repeat center center, 
-#62BC0F /* Color del botón */; background-size: 110%; height: 55px;  /* Alto del botón */
-  width: 110px;  /* Ancho del botón */"class="leekids" ></a>
+
+      <a href="../menu.php" style=" background: url(Imagenes/041-home.png) no-repeat center center;
+ background-size: 50%; height: 75px;
+  width: 110px; margin:70px 50px;" class="leekids" ></a>
 </section>
 </body>
 </html>
